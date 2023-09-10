555510080ae6c5935ec02c79da66968f6c1f65de
JitendraYadav <Jitendrayadav91001@gmail.com>
2023-09-10 21:34:25 +0530

    add lazy

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,17 @@ import React, { Suspense, lazy } from "react";
 import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
 import BeatLoader from "react-spinners/BeatLoader";
 import "./app.scss";
-// import Home from "./Pages/Home/Home";
-// import Products from "./Pages/Products/Products";
-// import Product from "./Pages/Product/Product";
-// import Navbar from "./Component/Navbar/Navbar";
-// import Footer from "./Component/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import Product from "./Pages/Product/Product";
+import Navbar from "./Component/Navbar/Navbar";
+import Footer from "./Component/Footer/Footer";
 
-const Home = lazy(() => import("./Pages/Home/Home"));
-const Products = lazy(() => import("./Pages/Products/Products"));
-const Product = lazy(() => import("./Pages/Product/Product"));
-const Navbar = lazy(() => import("./Component/Navbar/Navbar"));
-const Footer = lazy(() => import("./Component/Navbar/Navbar"));
+// const Home = lazy(() => import("./Pages/Home/Home"));
+// const Products = lazy(() => import("./Pages/Products/Products"));
+// const Product = lazy(() => import("./Pages/Product/Product"));
+// const Navbar = lazy(() => import("./Component/Navbar/Navbar"));
+// const Footer = lazy(() => import("./Component/Navbar/Navbar"));
 function App() {
   const Layout = () => {
     return (
@@ -48,9 +48,9 @@ function App() {
   ]);
   return (
     <>
-      <Suspense fallback={<BeatLoader color="#36d7b7" style={{display:"flex",justifyContent:"center",alignItems:'center'}} />}>
+      {/* <Suspense fallback={<BeatLoader color="#36d7b7" style={{display:"flex",justifyContent:"center",alignItems:'center'}} />}> */}
         <RouterProvider router={router} />
-      </Suspense>
+      {/* </Suspense> */}
     </>
   );
 }
